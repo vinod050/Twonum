6c92735d1633eda149c503f00f302ee5419b4f79
vinod kumar <sirikondaviinod99@gmail.com>
2025-01-29 17:19:47 +0530

    Add files via upload

---
+++ Twonum.java
@@ -0,0 +1,17 @@
+public class Twonum {
+    public static void main(String[] args) {
+        int a=25;
+        int b=30;
+        int sum=a+b;
+        int sub=a-b;
+        int multi=a*b;
+        float division=a/b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(division);
+
+    }
+    
+}
